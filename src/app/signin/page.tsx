147f537c505a91543f5be03060f48e4f315af2dd
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-10-12 13:23:44 +0500

    fix signin page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -17,7 +17,7 @@ export default function SignIn() {
       email,
       password,
     })
-    
+
     console.log(data)
     if (error) {
       setError(error.message)
@@ -30,29 +30,30 @@ export default function SignIn() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 p-4 sm:p-6 lg:p-8">
-      <div className="bg-white dark:bg-gray-800 p-6 sm:p-8 lg:p-10 rounded-md shadow-md max-w-sm sm:max-w-md lg:max-w-lg w-full">
+      <div className="bg-white dark:bg-gray-800 p-6 sm:p-8 lg:p-10 rounded-md shadow-md max-w-sm sm:max-w-md lg:max-w-lg  w-full">
         <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-gray-700 dark:text-white mb-6 text-center">Sign In</h1>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="w-full p-3 sm:p-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="w-full p-3 sm:p-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-        />
-        {error && <p className="text-red-500 text-sm sm:text-base mb-4">{error}</p>}
-        <button
-          onClick={handleSignIn}
-          className="w-full p-3 sm:p-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors duration-200"
-        >
-          Sign In
-        </button>
+        <form onSubmit={handleSignIn} className="flex flex-col gap-4">
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full p-3 sm:p-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full p-3 sm:p-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+          />
+          {error && <p className="text-red-500 text-sm sm:text-base mb-4">{error}</p>}
+          <button type="submit"
+            className="w-full p-3 sm:p-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors duration-200"
+          >
+            Sign In
+          </button>
+        </form>
         <p className="text-sm sm:text-base text-gray-600 dark:text-gray-400 mt-4 text-center">
           Don&apos;t have an account?{' '}
           <a href="/signup" className="text-blue-500 hover:underline">
